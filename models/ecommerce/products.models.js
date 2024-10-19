31b0c0059827459fb57777d543e838465952cdd1
Md. Sakib Hasan <sakib3046@gmail.com>
2024-10-19 14:01:39 +0600

    template done

---
+++ models/ecommerce/products.models.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+const productSchema = new mongoose.Schema(
+    {
+
+    },
+    {timestamps:true}
+)
+
+export const Product = mongoose.model("Product",productSchema)
