ae5f71fb7aa1627d6a40160fb722fa2782637e85
Md. Sakib Hasan <sakib3046@gmail.com>
2024-10-21 01:48:54 +0600

    ecommerce product

--- models/ecommerce/order.models.js
+++ models/ecommerce/order.models.js
@@ -1,7 +1,23 @@
 import mongoose from "mongoose";
 const orderSchema = new mongoose.Schema(
     {
-
+        orderPrice:{
+            type:Number,
+            required:true,
+        },
+        customer:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        },
+        orderItems:{
+            type:[],
+            required:true,
+        },
+        category:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"Category",
+            required:true,
+        },
     },
     {timestamps:true}
 )
--- models/ecommerce/products.models.js
+++ models/ecommerce/products.models.js
@@ -1,7 +1,34 @@
 import mongoose from "mongoose";
 const productSchema = new mongoose.Schema(
     {
-
+        name:{
+            type:String,
+            required:true,
+        },
+        description:{
+            type:String,
+            required:true,
+        },
+        productImage:{
+            type:String
+        },
+        price:{
+            type:Number,
+            default:0
+        },
+        stock:{
+            type:Number,
+            default:0
+        },
+        category:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"Category",
+            required:true,
+        },
+        owner:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        }
     },
     {timestamps:true}
 )
